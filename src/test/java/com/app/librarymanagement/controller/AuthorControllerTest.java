87974856e3cb3cf2f30a81a0f4c6dc7a9bcc6b2a
jyoti92173 <jyoti53913@gmail.com>
2025-01-02 16:37:37 +0530

    Added unit tests for AuthorService CRUD methods

--- src/test/java/com/app/librarymanagement/controller/AuthorControllerTest.java
+++ src/test/java/com/app/librarymanagement/controller/AuthorControllerTest.java
@@ -5,12 +5,13 @@ import com.app.librarymanagement.repo.AuthorRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class AuthorControllerTest {
 
     @Test
