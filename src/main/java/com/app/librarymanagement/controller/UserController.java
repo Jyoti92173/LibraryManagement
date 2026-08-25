dd8cc49fcfcd8051a17508facf001f46253fc4ae
jyoti92173 <Jyoti53913@gmail.com>
2026-08-25 22:51:18 +0530

    Create User CRUD APIs.

--- src/main/java/com/app/librarymanagement/controller/UserController.java
+++ src/main/java/com/app/librarymanagement/controller/UserController.java
@@ -20,7 +20,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
-
     @Autowired
     private UserService userService;
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
