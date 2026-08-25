9ae13353de37feee2814293eb23a1592efcdaec1
jyoti92173 <Jyoti53913@gmail.com>
2026-08-25 22:41:08 +0530

    Add Book Entity with field

--- src/main/java/com/app/librarymanagement/entity/Book.java
+++ src/main/java/com/app/librarymanagement/entity/Book.java
@@ -1,10 +1,12 @@
 package com.app.librarymanagement.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Objects;
 
 @Entity
+@Data
 @Table(name = "book")
 public class Book{
 
@@ -27,88 +29,5 @@ public class Book{
     @JoinColumn(name = "publisher_id")
     private Publisher publisher;
 
-    public Book(int bookId, int cost, Author author, Publisher publisher, String title) {
-        this.bookId = bookId;
-        this.cost = cost;
-        this.author = author;
-        this.publisher = publisher;
-        this.title = title;
-    }
 
-    public Book(String title, int cost, Publisher publisher, Author author) {
-        this.title = title;
-        this.cost = cost;
-        this.publisher = publisher;
-        this.author = author;
-    }
-
-    public Book() {
-    }
-
-    public int getBookId() {
-        return bookId;
-    }
-
-    public void setBookId(int bookId) {
-        this.bookId = bookId;
-    }
-
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-
-    public int getCost() {
-        return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
-
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
-
-
-    public Publisher getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(Publisher publisher) {
-        this.publisher = publisher;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return bookId == book.bookId && cost == book.cost && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(publisher, book.publisher);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(bookId, title, cost, author, publisher);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "bookId=" + bookId +
-                ", title='" + title + '\'' +
-                ", cost=" + cost +
-                ", author=" + author +
-                ", publisher=" + publisher +
-                '}';
-    }
 }
